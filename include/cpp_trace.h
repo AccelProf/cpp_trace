b6904e43390fd3fc93c28b1dcf1c34156052d1e8
Lin-Mao <linmaosdu@gmail.com>
2025-03-16 12:08:57 -0700

    Update

--- include/cpp_trace.h
+++ include/cpp_trace.h
@@ -1,5 +1,5 @@
-#ifndef __cpp_trace_H__
-#define __cpp_trace_H__
+#ifndef __CPP_TRACE_H__
+#define __CPP_TRACE_H__
 #include <string>
 #include <vector>
 
@@ -7,4 +7,4 @@ extern "C" bool init_backtrace(const char *filename);
 extern "C" std::string print_backtrace(int verbose = 1);
 extern "C" std::vector<std::string> get_backtrace(int keep = -1);
 
-#endif // __cpp_trace_H__
+#endif // __CPP_TRACE_H__
